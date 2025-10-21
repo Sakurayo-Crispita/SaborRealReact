a0e80ea3c8ec3fb55c7ceb23f4def8408aab8748
Crispita <sagato31@outlook.com>
2025-10-21 11:26:25 -0500

    Error en security.py

--- backend/app/security.py
+++ backend/app/security.py
@@ -1,3 +1,4 @@
+# backend/app/security.py
 from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["pbkdf2_sha256", "bcrypt"], deprecated="auto")
 
@@ -6,3 +7,6 @@ def hash_password(plain: str) -> str:
 
 def verify_password(plain: str, hashed: str) -> bool:
     return pwd_context.verify(plain, hashed)
+
+def get_password_hash(plain: str) -> str:
+    return hash_password(plain)
